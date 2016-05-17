a2df0eb9b2a1a1510a368de4e0443155ab4fb544
dariomas <dariomas@users.noreply.github.com>
2016-05-17 15:30:52 +0200

    More infos logged about Telegram bot connection; Tested only on Python 2.6 with future unicode

--- nagios_telegram.py
+++ nagios_telegram.py
@@ -9,7 +9,7 @@ warnings.filterwarnings("ignore", category=DeprecationWarning)
 import urllib3.contrib.pyopenssl
 urllib3.contrib.pyopenssl.inject_into_urllib3()
 from requests import Request, Session
-
+s = Session()
 import logging
 import logging.handlers
 LOG_FILENAME = '/tmp/telegram.log'
@@ -24,12 +24,12 @@ formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(messag
 handler.setFormatter(formatter)
 # add ch to logger
 logger.addHandler(handler)
-
 #logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s',level=logging.DEBUG)
 # Don't show logging messages while in production
 #logging.disable(logging.DEBUG)
 import sys
 import inspect
+
 # functions
 def whoami():
     return inspect.stack()[1][3]
@@ -49,6 +49,7 @@ def send_message(token, params):
     :rtype: HTTP code
 
     """
+    # required args
     logger.debug('received a call to %s', whoami())
     api_method = 'sendMessage'
     api_url_base = 'https://api.telegram.org/bot'
@@ -57,10 +58,75 @@ def send_message(token, params):
 
     #print(get_url, params)
     logger.debug("%s %s", get_url, params)
-    s = Session()
+    #s = Session()
     request = Request('POST', get_url, data=params).prepare()
     resp = s.send(request)
     logger.debug("%s", resp.status_code)
+    return resp.status_code
+
+
+def send_chat_action(token, params):
+    """
+    Use this method when you need to tell the user that something is happening on the bot's side. The status is set
+    for 5 seconds or less (when a message arrives from your bot, Telegram clients clear its typing status).
+    Example: The ImageBot needs some time to process a request and upload the image. Instead of sending a text message
+    along the lines of “Retrieving image, please wait…”, the bot may use sendChatAction with action = upload_photo.
+    The user will see a “sending photo” status for the bot.
+    We only recommend using this method when a response from the bot will take a noticeable amount of time to arrive.
+
+    :param chat_id: Unique identifier for the message recipient — User or GroupChat id
+    :param action: Type of action to broadcast.  Choose one, depending on what the user is about to receive:
+     typing for text messages, upload_photo for photos, record_video or upload_video for videos,
+     record_audio or upload_audio for audio files, upload_document for general files,
+     find_location for location data.
+    :param token: The API token generated following the instructions at https://core.telegram.org/bots#botfather
+    :param params: A dictionary mapping the api method parameters to their arguments
+
+    :type chat_id: int
+    :type action: ChatAction
+
+    :returns: Returns True on success.
+    :rtype: HTTP code
+
+    """
+    # required args
+    #params = dict(chat_id=chat_id, action=action)
+
+    logger.debug('received a call to %s', whoami())
+    api_method = 'sendChatAction'
+    api_url_base = 'https://api.telegram.org/bot'
+
+    get_url = '{base_url}{token}/{method}'.format(base_url=api_url_base, token=token, method=api_method)
+
+    #print(get_url, params)
+    logger.debug("%s %s", get_url, params)
+    #s = Session()
+    request = Request('POST', get_url, data=params).prepare()
+    resp = s.send(request)
+    logger.debug("%s", resp.status_code)
+    return resp.status_code
+
+
+def get_me(token):
+    """
+    A simple method for testing your bot's auth token. Requires no parameters.
+    Returns basic information about the bot in form of a User object.
+    :param \*\*kwargs: Args that get passed down to :class:`TelegramBotRPCRequest`
+    :returns: Returns basic information about the bot in form of a User object.
+    :rtype: User
+    """
+    logger.debug('received a call to %s', whoami())
+    api_method = 'getMe'
+    api_url_base = 'https://api.telegram.org/bot'
+
+    get_url = '{base_url}{token}/{method}'.format(base_url=api_url_base, token=token, method=api_method)
+
+    #print(get_url, params)
+    logger.debug("%s ", get_url)
+    #s = Session()
+    request = Request('POST', get_url).prepare()
+    resp = s.send(request)
+    logger.debug("%s %s", resp.status_code, resp.text)
     return resp.status_code
 
 
@@ -163,6 +229,9 @@ def main():
     args = parse_args()
     logger.debug(args)
     user_id = int(args.contact)
+    get_me(args.token)
+    #params = dict(chat_id=user_id, action='find_location')
+    send_chat_action(args.token, dict(chat_id=user_id, action='find_location'))
     if args.object_type == 'host':
         message = host_notification(args)
     elif args.object_type == 'service':
